4e45bf3675998fd9866eeb9b3523c0fbcb9ba4d4
Don2kay <kabiradeyemo@gmail.com>
2022-10-13 09:23:25 +0100

    carousel slider

---
+++ script.js
@@ -0,0 +1,41 @@
+//Hint
+//Critically Examine the mark up and the CSS
+//Select the left and right buttons
+//Select element with id of imgs
+//select all the images
+//crate a function to change image on button click
+//you may need to learn about about the setInterval method
+//you will need to transform the images offscreen to implement sliding effect
+//Goodluck😎 
+
+"use strict";
+
+let images = document.getElementsByTagName("img"),
+    imgCon = document.querySelector("#imgs"),
+    starter = 0;
+
+
+function display(){
+  for(let i = 0; i < images.length; i++){
+    images[i].style.display= "none";
+  }
+  images[starter].style.display= "block";
+};
+
+function rightSlider(){
+  if (starter == images.length - 1) {
+    starter = 0;
+  } else {
+    starter++;
+  };
+  display();
+};
+
+function leftSlider(){
+  if (starter == 0) {
+    starter = images.length - 1;
+  } else {
+    starter--;
+  };
+  display();
+};
