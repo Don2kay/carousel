50597adffb82d33e529477f1d5f97882da58af52
Don2kay <kabiradeyemo@gmail.com>
2022-10-13 10:12:08 +0100

    new update

--- script.js
+++ script.js
@@ -10,19 +10,20 @@
 
 "use strict";
 
+//On click method
+
 let images = document.getElementsByTagName("img"),
     imgCon = document.querySelector("#imgs"),
     starter = 0;
 
-// Function for looping through images
+// // Function for looping through images
 function display(){
   for(let i = 0; i < images.length; i++){
-    images[i].style.display= "none";
-  }
-  images[starter].style.display= "block";
+    imgCon.style.transform = `translate(${-starter * 500}px)`;
+  };
 };
 
-// Function for the next button
+// // Function for the next button
 function rightSlider(){
   if (starter == images.length - 1) {
     starter = 0;
@@ -32,7 +33,7 @@ function rightSlider(){
   display();
 };
 
-// Function for the prev button
+// // Function for the prev button
 function leftSlider(){
   if (starter == 0) {
     starter = images.length - 1;
@@ -41,3 +42,28 @@ function leftSlider(){
   };
   display();
 };
+
+// AddEventListener Method
+// const leftBtn = document.querySelector("#left"),
+//       rightBtn = document.querySelector("#right"),
+//       imageCon = document.querySelector("#imgs"),
+//       images = document.querySelectorAll("img");
+// let counter = 0;
+
+// const changeImage = function(){
+//   if(counter > images.length - 1){
+//     counter = 0;
+//   } else if (counter < 0){
+//     counter = images.length - 1;
+//   }
+//   imageCon.style.transform = `translate(${-counter * 500}px)`
+// };
+
+// rightBtn.addEventListener("click", function(){
+//   counter++
+//   changeImage();
+// })
+// leftBtn.addEventListener("click", function(){
+//   counter--
+//   changeImage();
+// });
